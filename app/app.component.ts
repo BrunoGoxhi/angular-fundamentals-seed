abf05f10287667f289ca65b4f6e88c927a698713
bgoxhi <bruno_goxhi@hotmail.com>
2019-08-25 17:12:24 +0200

    Rendering flows chapter: Lesson 13-16 ng-if, ngFor, ngStyle

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,27 +1,77 @@
 import {Component } from '@angular/core'
 
+interface Passenger {
+  id: number,
+  fullName: string,
+  checkedIn: boolean,
+  checkInDate: null | number
+}
+
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
-    <div class="app">
-      <button (click)=handleClick(username.value)>
-        Get Value
-      </button>
-      <input type="text" #username />
-      <div>
-        {{ name }}
-      </div>
+    <div class="app">  
+    <h1>Pasagjeret e linjes: </h1>
+    <ul>
+      <li *ngFor = "let Passenger of passengers;">
+        <span 
+        class="status"
+        [class.checked-in]="Passenger.checkedIn"
+        ></span>
+          {{ Passenger.fullName }}
+      </li>
+    </ul>
+      <h1>Pasagjeret e linjes: </h1>
+      <ul>
+        <li *ngFor = "let Passenger of passengers;">
+          <span 
+          class="status"
+          [ngClass]="{
+            'checked-in': Passenger.checkedIn,
+            'checked-out': !Passenger.checkedIn
+          }"
+          ></span>
+            {{ Passenger.fullName }}
+        </li>
+      </ul>
+      <h1>Pasagjeret e linjes: </h1>
+      <ul>
+        <li *ngFor = "let Passenger of passengers;">
+          <span 
+          class="status"
+          [style.backgroundColor]="(Passenger.checkedIn ? '#22eb75' : '#c0392b')"
+          ></span>
+            {{ Passenger.fullName }}
+        </li>
+      </ul>
+      <h1>Pasagjeret e linjes: </h1>
+      <ul>
+        <li *ngFor = "let Passenger of passengers;">
+          <span 
+          class="status"
+          [ngStyle]="{ backgroundColor: (Passenger.checkedIn ? '#22eb75' : '#c0392b')}"
+          ></span>
+            {{ Passenger.fullName }}
+        </li>
+      </ul>
     </div>
     `
 })
 
 export class AppComponent {
-  name: string = "Bruno";
-
-  handleClick(value: string)
+  passengers: Passenger[] = 
+  [{
+    id: 1,
+    fullName: 'Bristol',
+    checkedIn: true,
+    checkInDate: 1233221312
+  },
   {
-    console.log(value);
+    id: 2,
+    fullName: 'Cash',
+    checkedIn: false,
+    checkInDate: null
   }
-
+  ]
 }  
