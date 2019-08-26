916f11970f989f617c7d315c1e7ed83ee09dca3c
Bruno Goxhi <bruno.goxhi@ikubinfo.al>
2019-08-26 09:57:04 +0200

    Not used modules removed from module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule} from '@angular/common';
-import { FormsModule} from '@angular/forms';
 
 import { AppComponent} from './app.component';
 
@@ -10,9 +8,7 @@ import { AppComponent} from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    CommonModule,
-    FormsModule 
+    BrowserModule
   ],
   bootstrap: [AppComponent]
 })
